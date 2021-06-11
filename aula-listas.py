bf6c4c880c26b0db08ff728c8d9dd13233a36dd4
Samuel Fernandes <samueldiablo73@gmail.com>
2021-06-11 17:21:23 -0300

    ambiente virual criado -- aprendizado python em producao

---
+++ aula-listas.py
@@ -0,0 +1,27 @@
+lista = [1, 3, 5, 7]
+lista_animais = ['cachorro', 'gato', 'rato']
+
+# nova_lista = lista_animais * 3
+
+# print(nova_lista)
+
+print(lista_animais)
+
+if 'lobo' in lista_animais:
+    print('exite o animal')
+else:
+    print('não exite o animal')
+    lista_animais.append('lobo')
+
+print(lista_animais)
+
+# print(lista_animais)
+
+# print(sum(lista))
+
+# print(max(lista))
+
+# print(max(lista_animais))
+
+# for x in lista_animais:
+#     print(x)
---
+++ aula.py
@@ -0,0 +1,170 @@
+
+# a = 0
+
+# while a <= 10:
+#     print(a)
+#     a += 1
+
+
+# n1 = float(input('Informe nota 1: '))
+
+# while n1 > 10 or n1 < 0:
+#     n1 = float(input('Você digitou a nota erra: Informe N1: '))
+
+
+# n2 = float(input('Informe nota 2: '))
+# while n2 > 10 or n2 < 0:
+#     n2 = float(input('Você digitou a nota erra: Informe N2: '))
+
+
+# n3 = float(input('Informe nota 3: '))
+# while n3 > 10 or n3 < 0:
+#     n3 = float(input('Você digitou a nota erra: Informe N3: '))
+
+
+# n4 = float(input('Informe nota 4: '))
+# while n4 > 10 or n4 < 0:
+#     n4 = float(input('Você digitou a nota erra: Informe N4: '))
+
+
+# media = (n1 + n2 + n3 + n4)/4
+
+# print('Média: {}'.format(media))
+
+
+# if(media >= 7):
+#     print('Você foi aprovado!')
+# elif(media >= 5):
+#     print('Você está na final')
+# else:
+#     print('Você foi reprovado')
+
+# for x in range(1, 101):
+#     print(x)
+
+# a = int(input('Informe um Nº: '))
+
+# div = 0
+
+# for x in range(1, a + 1):
+#     resto = a % x
+#     print(x, resto)
+
+#     if resto == 0:
+#         div += 1
+
+# if div == 2:
+#     print('é primo {}'.format(a))
+# else:
+#     print('não é primo {}'.format(a))
+
+# a = int(input('Informe o valor: '))
+
+# for i in range(a + 1):
+
+#     div = 0
+
+#     for x in range(1, i+1):
+#         resto = i % x
+
+#         if resto == 0:
+#             div += 1
+
+#     if div == 2:
+#         print('É primo {}'.format(i))
+#     else:
+#         print('não é primo {}'.format(i))
+
+
+# resultado = 0
+# for x in range(1, 10):
+#     if x < 9:
+#         resultado += 1
+
+
+# n1 = float(input('Informe o dado: '))
+
+# n2 = float(input('Informe o dado: '))
+
+# n3 = float(input('Informe o dado: '))
+
+# media = (n1 + n2 + n3)/3
+
+# if(media >= 7):
+#     print('aprovado')
+# elif(media >= 5):
+#     print('prova final')
+# else:
+#     print('reprovado')
+
+
+# a = int(input('Primeiro valor A: '))
+
+# b = int(input('Segundo valor B: '))
+
+# c = int(input('Segundo valor C: '))
+
+# if a > b and a > c:
+#     print('O maior número é A = {}'.format(a))
+# elif(b > a > c):
+#     print('O maior número é B = {}'.format(b))
+# else:
+#     print('O maior numero é C = {}'.format(c))
+
+# a = int(input('Informe um número: '))
+
+# if a % 2 == 0:
+#     print('O numero é par {}'.format(a))
+# else:
+#     print('O número é ímpar: {}'.format(a))
+
+# numero par / impar
+
+# a = int(input('Informe um numero: '))
+
+# b = int(input('Informe um numero: '))
+
+# resto_a = a % 2
+
+# resto_b = b % 2
+
+# # not inverte a condicao
+
+# if resto_a == 0 or not resto_b > 0:
+#     print('Tem número par')
+# else:
+#     print('Não tem número par apenas impar')
+
+# Media de alunos
+
+# n1 = float(input('Informe nota 1: '))
+
+# while n1 > 10 or n1 < 0:
+#     n1 = float(input('Você digitou a nota erra: Informe N1: '))
+
+
+# n2 = float(input('Informe nota 2: '))
+# while n2 > 10 or n2 < 0:
+#     n2 = float(input('Você digitou a nota erra: Informe N2: '))
+
+
+# n3 = float(input('Informe nota 3: '))
+# while n3 > 10 or n3 < 0:
+#     n3 = float(input('Você digitou a nota erra: Informe N3: '))
+
+
+# n4 = float(input('Informe nota 4: '))
+# while n4 > 10 or n4 < 0:
+#     n4 = float(input('Você digitou a nota erra: Informe N4: '))
+
+
+# media = (n1 + n2 + n3 + n4)/4
+
+# print('Média: {}'.format(media))
+
+# if(media >= 7):
+#     print('aprovado')
+# elif(media >= 5):
+#     print('prova final')
+# else:
+#     print('reprovado')
