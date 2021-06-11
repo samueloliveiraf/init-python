d0b0010c52ff52333585f3cdaa82de451000a1dd
Samuel Fernandes <samueldiablo73@gmail.com>
2021-06-11 12:40:51 -0300

    estudando Python

--- primeiro-aula/aula2.py
+++ primeiro-aula/aula2.py
@@ -1,3 +1,16 @@
 
+n1 = float(input('Informe o dado: '))
 
+n2 = float(input('Informe o dado: '))
+
+n3 = float(input('Informe o dado: '))
+
+media = (n1 + n2 + n3)/3
+
+if(media >= 7):
+    print('aprovado')
+elif(media >= 5):
+    print('prova final')
+else:
+    print('reprovado')
 
--- primeiro-aula/index.py
+++ primeiro-aula/index.py
@@ -4,6 +4,7 @@ b = 10 % 2
 print(a, b)
 
 soma = 5 + 5
+
 print('soma: {}'.format(soma))
 
 a = int(input('Informe o dado: '))
@@ -14,18 +15,5 @@ s = a * b
 
 print(s)
 
-n1 = float(input('Informe o dado: '))
 
-n2 = float(input('Informe o dado: '))
-
-n3 = float(input('Informe o dado: '))
-
-media = (n1 + n2 + n3)/3
-
-if(media >= 7):
-    print('aprovado')
-elif(media >= 5):
-    print('prova final')
-else:
-    print('reprovado')
     
