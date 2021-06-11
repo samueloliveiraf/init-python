d800c8a8916d2ef0906d1da794d552235e0085b7
Samuel Fernandes <samueldiablo73@gmail.com>
2021-06-10 23:19:12 -0300

    iniciando em python

---
+++ primeiro-aula/index.py
@@ -0,0 +1,31 @@
+
+a = 10 / 2
+b = 10 % 2
+print(a, b)
+
+soma = 5 + 5
+print('soma: {}'.format(soma))
+
+a = int(input('Informe o dado: '))
+
+b = int(input('Informe o dado: '))
+
+s = a * b
+
+print(s)
+
+n1 = float(input('Informe o dado: '))
+
+n2 = float(input('Informe o dado: '))
+
+n3 = float(input('Informe o dado: '))
+
+media = (n1 + n2 + n3)/3
+
+if(media >= 7):
+    print('aprovado')
+elif(media >= 5):
+    print('prova final')
+else:
+    print('reprovado')
+    
